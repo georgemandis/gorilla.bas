f3ded5a4f978faca2ff03fa1e958fd03e2bb7a56
George Mandis <george@mand.is>
2026-05-16 20:32:58 -0400

    fix: use consume_step_delta() instead of non-existent step_delta property

--- src/input.ts
+++ src/input.ts
@@ -28,7 +28,7 @@ export function getPlayerInput(player: 1 | 2): PlayerInput {
     dpadRight: classic.DPAD.right,
     a: classic.A,
     b: classic.B,
-    spinnerDelta: spinner.SPINNER.step_delta * DEGREES_PER_STEP,
+    spinnerDelta: spinner.SPINNER.consume_step_delta() * DEGREES_PER_STEP,
   };
 }
 
